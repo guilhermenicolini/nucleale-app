11ef0e38de2f55ea94d900a162529987f0d95bff
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-07-08 17:38:25 -0300

    chore: fix storybook alias

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,9 +9,17 @@ module.exports = {
   webpackFinal: async (config) => {
     const merged = {
       ...config,
+      cache: false,
       module: {
         ...config.module,
         rules: custom.module.rules
+      },
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve?.alias,
+          ...custom.resolve.alias
+        }
       }
     }
     return merged
