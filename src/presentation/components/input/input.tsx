961227484bc1c1969141fdcf22d18adfe726211d
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-07-09 08:25:01 -0300

    feat: input styles and props

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,12 +1,16 @@
-import styled from 'styled-components'
-import { FC } from 'react'
-import { lighten } from '@/presentation/styles/utils'
+import styled, { Theme } from 'styled-components'
+import React, { FC } from 'react'
+import { BaseInput } from '@/presentation/components/base'
 
-interface InputProps {
+export interface InputProps {
+  theme: Theme
   label?: string
+  required?: boolean
   placeholder?: string
   value?: string
+  touched?: boolean
   error?: string
+  disabled?: boolean
 }
 
 const Wrapper = styled.div`
@@ -17,45 +21,30 @@ const Wrapper = styled.div`
 
 const Label = styled.label`
   ${(props) => props.theme.typography.subtitle1}
-  color: ${(props) => props.theme.colors.dark.highEmphasis}
-`
-
-const InputText = styled.input.attrs(() => ({
-  type: 'text'
-}))`
-  min-width: 230px;
-  height: 30px;
-  border-radius: ${(props) => props.theme.general.borderRadius};
-  border: 1px solid ${(props) => props.theme.colors.palette.form};
-  padding-left: 3px;
-  margin-bottom: 2px;
-
-  ::placeholder {
-    color: ${(props) => props.theme.colors.palette.placeHolder};
-  }
-  &:hover {
-    border-color: ${(props) => props.theme.colors.palette.primary};
-  }
-  &:enabled:focus {
-    box-shadow: 0 0 0 0.2rem ${(props) => lighten(props.theme.colors.palette.primary, 50)};
-    outline: 0 none;
-    outline-offset: 0;
-    border-color: ${(props) => props.theme.colors.palette.primary};
+  color: ${(props) => props.theme.colors.dark.highEmphasis};
+  &.invalid {
+    color: ${(props) => props.theme.colors.palette.danger};
   }
 `
 
-const Span = styled.span`
+const Span = styled.span<InputProps>`
   ${(props) => props.theme.typography.caption};
   color: ${(props) => props.theme.colors.palette.danger};
-  min-width: 13px;
+  min-height: 13px;
 `
 
-const Input: FC<InputProps> = ({ label, placeholder, value, error }: InputProps) => {
+const Input: FC<InputProps> = (props: InputProps) => {
+  const isInvalid = props.touched && props.error?.length > 0
   return (
     <Wrapper>
-      <Label>{label}</Label>
-      <InputText placeholder={placeholder} value={value} />
-      <Span>{error}</Span>
+      <Label className={isInvalid ? 'invalid' : ''}>{props.label}{props.required ? ' *' : '' }</Label>
+      <BaseInput
+        type="text"
+        placeholder={props.placeholder}
+        defaultValue={props.value}
+        className={isInvalid ? 'invalid' : ''}
+        disabled={props.disabled} />
+      <Span>{props.error}</Span>
     </Wrapper>
   )
 }
