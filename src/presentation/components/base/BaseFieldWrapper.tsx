b0b4a3d7c96c5868241085257253229b8b84ca54
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-07-10 20:17:28 -0300

    feat: add input icon

--- src/presentation/components/base/BaseFieldWrapper.tsx
+++ src/presentation/components/base/BaseFieldWrapper.tsx
@@ -4,4 +4,11 @@ export const BaseFieldWrapper = styled.div`
   margin-bottom: 8px;
   display: flex;
   flex-direction: column;
+  position: relative;
+
+  & svg {
+    position: absolute;
+    top: 23px;
+    right: 5px;
+  }
 `
