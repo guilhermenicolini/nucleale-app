cbce13774c29d2309e343975991b24c0571313ed
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-07-22 14:58:19 -0300

    chore: change react-toastfy styles

--- src/presentation/styles/global.tsx
+++ src/presentation/styles/global.tsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle, Theme } from 'styled-components'
+import 'react-toastify/dist/ReactToastify.min.css'
 
 export const GlobalStyle = createGlobalStyle<{ theme: Theme }>`
   * {
@@ -7,5 +8,18 @@ export const GlobalStyle = createGlobalStyle<{ theme: Theme }>`
     padding: 0;
     margin: 0;
     box-sizing: border-box;
+
+    .Toastify__toast--info {
+      background-color: ${({ theme }) => theme.colors.palette.info.color};
+    }
+    .Toastify__toast--success {
+      background-color: ${({ theme }) => theme.colors.palette.success.color};
+    }
+    .Toastify__toast--warning {
+      background-color: ${({ theme }) => theme.colors.palette.warning.color};
+    }
+    .Toastify__toast--error {
+      background-color: ${({ theme }) => theme.colors.palette.danger.color};
+    }
   }
 `
