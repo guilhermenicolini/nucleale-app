fa3f56539ce4a8b636e46026b87840c20aacef3f
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-07-23 11:18:56 -0300

    chore: remove alert component

--- src/presentation/components/alert/AlertMessage.tsx
+++
@@ -1,8 +0,0 @@
-import { contrast } from '@/presentation/styles/utils'
-import styled from 'styled-components'
-
-export const AlertMessage = styled.div`
-  ${(props) => props.theme.typography.body2};
-  padding: 10px;
-  color: ${(props) => contrast(props.theme.colors.palette[props.color].color)};
-`
--- src/presentation/components/toastr/Toastr.tsx
+++
@@ -1,8 +0,0 @@
-import { toast } from 'react-toastify'
-import Alert from '../alert/Alert'
-
-export default class Toastr {
-  show (message: string): void {
-    toast(<Alert color="success" message={message} />)
-  }
-}
