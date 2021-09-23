d97a1cb85ef7f87072a194a77b3c10b70cf7c360
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-09-23 09:59:59 -0300

    feat: bind validations to Login

--- src/presentation/pages/login/Login.tsx
+++ src/presentation/pages/login/Login.tsx
@@ -2,14 +2,24 @@ import React, { useState } from 'react'
 import { Public, Field, Button, LinkButton, Spinner } from '@/presentation/components'
 import * as S from './Login.styles'
 import { FormContext } from '@/presentation/contexts'
+import { useForm } from 'react-hook-form'
+
+type FormData = {
+  email: string
+  password: string
+}
 
 export const Login: React.FC = () => {
+  const { register, handleSubmit, formState: { isValid, isDirty, errors } } = useForm<FormData>(
+    { mode: 'all' }
+  )
+
   const [state, setState] = useState({
     isLoading: false,
-    formValid: false
+    formValid: true
   })
 
-  const submit = async (): Promise<void> => {
+  const submit = async (data: any): Promise<void> => {
     setState(s => ({ ...s, isLoading: true }))
     setTimeout(() => {
       setState(s => ({ ...s, isLoading: false }))
@@ -19,10 +29,24 @@ export const Login: React.FC = () => {
   return (
     <Public>
       <FormContext.Provider value={{ state, setState }}>
-        <S.Form>
-          <Field type="email" name="email" label="E-mail" placeholder="Informe seu e-mail" />
-          <Field type="password" name="password" label="Senha" placeholder="Informe sua senha" />
-          <Button type="submit" block disabled={!state.formValid} onClick={submit}>Entrar</Button>
+        <S.Form onSubmit={handleSubmit(submit)} noValidate>
+          <Field
+            label="E-mail"
+            placeholder="Informe seu e-mail"
+            name="email"
+            type="email"
+            {...register('email', { required: 'Campo obrigatório' })}
+            error={errors.email?.message}
+            touched={isDirty} />
+          <Field
+            label="Senha"
+            placeholder="Informe sua senha"
+            name="email"
+            type="password"
+            {...register('password', { required: 'Campo obrigatório' })}
+            error={errors.password?.message}
+            touched={isDirty} />
+          <Button type="submit" block disabled={!isValid} >Entrar</Button>
           <Button variant="secondary" block>Criar Conta</Button>
           <LinkButton>Esqueceu sua senha?</LinkButton>
         </S.Form>
