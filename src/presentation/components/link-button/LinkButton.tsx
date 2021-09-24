86d48993168e2a334afd9006da0d52ecd4d12fcd
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-09-24 14:35:00 -0300

    feat: add link button type

--- src/presentation/components/link-button/LinkButton.tsx
+++ src/presentation/components/link-button/LinkButton.tsx
@@ -4,12 +4,13 @@ import * as S from './LinkButton.styles'
 
 export type LinkButtonProps = {
   variant?: Variants
+  type?: 'button' | 'submit'
   disabled?: boolean
   children?: React.ReactNode
 }
 
 export const LinkButton: FC<LinkButtonProps> = (props: LinkButtonProps) => {
   return (
-    <S.LinkButton disabled={props.disabled} variant={props.variant} >{props.children}</S.LinkButton>
+    <S.LinkButton type={props.type || 'button'} disabled={props.disabled} variant={props.variant} >{props.children}</S.LinkButton>
   )
 }
