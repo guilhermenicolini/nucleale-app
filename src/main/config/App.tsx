dedf4e0c6774d6ecf1f2c838c1e13e34752cdf2b
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-09-26 11:32:18 -0300

    feat: initiate App with ApiContext

--- src/main/config/App.tsx
+++ src/main/config/App.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme, Normalize, GlobalStyle } from '@/presentation/styles'
 import { ToastContainer } from '@/presentation/components'
+import { ApiContext } from '@/presentation/contexts'
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '../adapters'
 
 export type AppProps = {
   children?: React.ReactNode
@@ -9,12 +11,18 @@ export type AppProps = {
 
 const App: FC<AppProps> = (props: AppProps) => {
   return (
-    <ThemeProvider theme={defaultTheme}>
-    <Normalize />
-    <GlobalStyle />
-    {props.children}
-    <ToastContainer />
-  </ThemeProvider>
+    <ApiContext.Provider
+      value={{
+        setCurrentAccount: setCurrentAccountAdapter,
+        getCurrentAccount: getCurrentAccountAdapter
+      }}>
+      <ThemeProvider theme={defaultTheme}>
+        <Normalize />
+        <GlobalStyle />
+        {props.children}
+        <ToastContainer />
+      </ThemeProvider>
+    </ApiContext.Provider>
   )
 }
 
