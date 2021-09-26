4086c404bef56e56c1912bd8667fc88633893a8b
Guilherme Nicolini <guilhermenicolini@gmail.com>
2021-09-26 11:36:34 -0300

    feat: ensure PrivateRoute protects route

---
+++ src/presentation/components/private-route/PrivateRoute.tsx
@@ -0,0 +1,12 @@
+import { ApiContext } from '@/presentation/contexts'
+import React, { useContext } from 'react'
+import { Redirect, Route, RouteProps } from 'react-router'
+
+const PrivateRoute: React.FC<RouteProps> = (props: RouteProps) => {
+  const { getCurrentAccount } = useContext(ApiContext)
+  return getCurrentAccount()?.accessToken
+    ? <Route {...props} />
+    : <Route {...props} component={() => <Redirect to='/login' />} />
+}
+
+export default PrivateRoute
